a5535705886a6d1cfd5b9f339272f5bf71c11bbd
Vikas Varma Pokala <vikasvarma3412@gmail.com>
2023-07-12 18:04:17 -0400

    Commiting code and example

---
+++ Code.java
@@ -0,0 +1,9 @@
+public class Code{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result = a * b;
+        System.out.println(result);
+        System.out.println("Hello Git");
+    }
+}
